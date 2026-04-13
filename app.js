21911ee3d1a3b44bfa598fe892d697da535744a0
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2026-04-13 17:38:21 +0100

    added requestTime property to the req object during our example middleware in app.js

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ export const app = express();
 
 // set up middleware
 app.use(express.json());
+//just as an example of things we can do...
 app.use((req, res, next) => {
   console.log('Hello from our first middleware');
+  //we can even add properties to the request obj in here and they will be available in all the other middlewares and route handlers that come after this one in the 'pipeline'
+  req.requestTime = new Date().toISOString();
   next();
 });
 // mount our tour router which is now in its own file (in the routes folder) and all of it's handlers (which are known as controllers in the MVC pattern) are in the controllers folder
