d277b284eaf5462ff69abe635c3c1422a0a6fadf
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2026-04-14 13:38:47 +0100

    middleware to check for valid id on those routes that use it and check body for price and name on the post route

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ export const app = express();
 app.use(express.json());
 //just as an example of things we can do...
 app.use((req, res, next) => {
-  console.log('Hello from our first middleware');
+  //   console.log('Hello from our first middleware');
   //we can even add properties to the request obj in here and they will be available in all the other middlewares and route handlers that come after this one in the 'pipeline'
   req.requestTime = new Date().toISOString();
   next();
 });
-// mount our tour router which is now in its own file (in the routes folder) and all of it's handlers (which are known as controllers in the MVC pattern) are in the controllers folder
+// 'mount' our tour router which is now in its own file (in the routes folder) and all of it's handlers (which are known as controllers in the MVC pattern) are in the controllers folder
 app.use('/api/v1/tours', tourRouter);
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,6 +6,34 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );
 
+export const checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  //I think this is better than just checking the length of the array and I also couldn't understand why we were still using req.params.id instead of val in the course video, I might discover the reason later or when I test it. As it stands this now blocks invalid id requests and if the id is valid it attaches it to the req object as req.thisTour.
+  const thisTour = tours.find((el) => el.id === +val);
+  if (!thisTour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour id invalid',
+    });
+  } else {
+    //I don't need else but I think it makes it more readable
+    req.thisTour = thisTour;
+    next();
+  }
+};
+
+//our validation middleware for post requests (see the post route for usage)
+export const checkBody = (req, res, next) => {
+  if (!req.body.price || !req.body.name) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  console.log('body of POST request checked and has price and name');
+  next();
+};
+
 export const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -24,12 +52,13 @@ export const createTour = (req, res) => {
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
-      if (err) {
-        res.status(500).json({
-          status: 'error',
-          message: 'Could not save the new tour',
-        });
-      }
+      //I'm not sure why but this throws an error every time even though the new tour is created
+      //   if (err) {
+      //     res.status(500).json({
+      //       status: 'error',
+      //       message: 'Could not save the new tour',
+      //     });
+      //   }
       res.status(201).json({
         status: 'success',
         data: {
@@ -41,17 +70,27 @@ export const createTour = (req, res) => {
 };
 
 export const getTourById = (req, res) => {
-  const thisTour = tours.find((el) => el.id === +req.params.id);
-  if (!thisTour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Tour id invalid',
-    });
-  }
+  //because it is now going through the checkID param middleware it will only get here if the id is valid and will have been added to the request object as req.tour
   res.status(200).json({
     status: 'success',
     data: {
-      tour: thisTour,
+      tour: req.thisTour,
     },
+  });
+};
+
+export const updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+};
+
+export const deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 };
