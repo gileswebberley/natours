0cc55f9268d1a81e3652ccb005b1511842281deb
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2026-04-20 16:40:48 +0100

    add sort functionality to the getAllTours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,17 +2,27 @@ import Tour from '../models/tourModel.js';
 
 export const getAllTours = async (req, res) => {
   try {
+    // we want to filter out the non filtering query parameters so we make a copy and delete the unwanted
     let filteredQuery = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete filteredQuery[el]);
     //Advanced filtering (gt, gte, lt, lte)
     // we use a regEx to find these operators and replace them with the mongoose version (ie with a $ at the beginning)
     let queryStr = JSON.stringify(filteredQuery);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`); // the \b denotes an exact match and the /g at the end denotes that we want to replace all instances rather than just the first which it would do without it
+    // the \b denotes an exact match and the /g at the end denotes that we want to replace all instances rather than just the first which it would do without it
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    //then pop it back as json into the filtered query object
     filteredQuery = JSON.parse(queryStr);
-    console.log(filteredQuery);
+    // console.log(filteredQuery);
 
     let query = Tour.find(filteredQuery);
+
+    //Sorting is the first non-filtering query we'll check for, we can then chain it onto our mongoose query object before we execute it with the await command. The query string for this is?sort=price for example, but we can also sort by multiple fields by separating them with a comma like this?sort=price,ratingsAverage. In the code we have to replace the comma with a space because that is how mongoose wants it. To make it descending rather than ascending simply add a minus sign to the front of the sort-by field name, eg ?sort=-price
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    }
+
     const tours = await query;
     if (tours.length === 0) {
       throw new Error('No tours found');
