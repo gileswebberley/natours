3fd2cc784df82302697466d67f40a7b7e47d8917
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2026-04-23 14:44:01 +0100

    further mongoose middleware examples added to our tourModel along with further validation options

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,11 @@ const tourSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      //validators
       required: [true, 'A tour must have a name'],
+      maxLength: [40, 'A tour name must be shorter than 41 characters'],
+      minLength: [3, 'A tour name must be more than 3 characters'],
+      //not actual validators
       unique: true,
       trim: true,
     },
@@ -23,10 +27,18 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      //we can use an enum to set possible values
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty can only be set as easy, medium, or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      //validators to set the range
+      min: [1, 'A rating must be at least 1'],
+      max: [5, 'A rating cannot be greater than 5'],
     },
     ratingsQuantity: {
       type: Number,
@@ -36,7 +48,23 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          //val is the value that is being created, remember that this keyword only works on creation not on update
+          //the function must return a boolean as to whether it has been passed
+          if (this) {
+            //check that the discount isn't more than the original price
+            return val < this.price;
+          }
+          //stop it crashing if we update with runValidators set to true
+          return true;
+        },
+        // notice the use of the mongoose template string in the curly braces
+        message: 'The discount of {VALUE} is more than the original price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
@@ -57,6 +85,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     //This is the schema-options object which we'll use to inject our virtual property into the results
@@ -70,6 +102,7 @@ const tourSchema = new mongoose.Schema(
 // Notice that we define the images field as an array of strings, this means we can have a varying amount of images connected to the tour and they will be stored as the string file path to the image we have stored. I think there is probably a way to do Blobs but we'll come to that later I believe
 // In mongoose apparently Date.now() will create a timestamp when we produce a new document rather than it being Date.now the property which would mean it was set to the time I'm producing this schema. For Date fields mongoose will try to parse the value provided, so we could pass it the string "2025-10-5" for example and it would convert that into an actual Date object to store in the DB (if it can't it will throw an error)
 // select is the same as it's method counterpart, namely you can set it to false so that it is never returned from the database - equivalent to query.select('-createdAt')
+// min and max can also be used to validate dates apparently
 
 //As an example of how to create virtual properties....
 tourSchema.virtual('durationInWeeks').get(function () {
@@ -87,8 +120,28 @@ tourSchema.pre('save', function () {
 });
 
 //Just for reference here's a post-document hook that logs the just saved document
-tourSchema.post('save', function (doc) {
-  console.log('The document we just saved to the DB was: ', doc);
+// tourSchema.post('save', function (doc) {
+//   console.log('The document we just saved to the DB was: ', doc);
+// });
+
+// For QUERY MIDDLEWARE - the this keyword now points to the query object. Like when we used the 'save hook' we will attach these to the 'find' hook, however that will only run on find() not on findById() or findOne() for example so you can actually pass a regEx in instead of the hook name. Here we want these to run on any query that starts with find. We've added a secretTour property to our schema and this will filter them out so they are not returned in the results
+tourSchema.pre(/^find/, function () {
+  this.find({ secretTour: { $ne: true } });
+  //now to measure the request time we'll try to add a start property to the query object (although I don't think it'll work because it's now immutable - NO, I was thinking of the Express req.query object which is completely different to the mongoose query object which is not frozen and is commonly used to pass information through the query middleware pipeline!)
+  this.start = Date.now();
+  console.log(`start query time: ${this.start}`);
+});
+
+//In the post-query hook we have access to all of the documents that have been returned by the query
+tourSchema.post(/^find/, function (docs) {
+  console.log(
+    `The query took ${Date.now() - this.start} milliseconds to retrieve ${docs.length} documents`,
+  );
+});
+
+//AGGREGATION MIDDLEWARE - in here the this keyword points to the aggregate method itself. When we worked with aggregation we learnt how to build the 'pipeline' which is an array of objects (stages). As with the query hook above we want to exclude our secret tours from our aggregation functions so we simply add an extra $match stage to our pipeline array
+tourSchema.pre('aggregate', function () {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
 });
 
 //Next let's create the model based on this schema (notice the capitalisation of the Name, this is a standard convention). This must come after the virtuals and hooks are defined
