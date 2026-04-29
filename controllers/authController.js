130a4e1e0386368b3dd31c6c19c539e5cd1fdf86
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2026-04-29 14:16:28 +0100

    Producing a JWT in signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 //Later in the course when we're dealing with security and authentication there is a comment about the original create implementation, apparently we will be setting up user roles and if we didn't do the changes then a vicious user could set their own role as admin and get the least restricted access to the database.
 export const signup = async (req, res) => {
@@ -10,9 +11,16 @@ export const signup = async (req, res) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
+  //login user on signup using jsonwebtoken package - Jose is another option btw
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
+  //It doesn't in the course but should we not send the client the expires-in time too?
   res.status(201).json({
     status: 'success',
+    token,
+    token_expires_in: process.env.JWT_EXPIRES_IN,
     data: {
       user: newUser,
     },
