e195cc52ad8d5d68163b88553f0e288e489c8630
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2026-07-16 18:45:58 +0100

    Email class implemented

--- utils/email.js
+++ utils/email.js
@@ -1,4 +1,70 @@
 import nodemailer from 'nodemailer';
+import pug from 'pug'; //for converting templates to html
+import { convert, compile } from 'html-to-text'; //for converting html into raw text
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//create an email class so we can use templates
+export class Email {
+  //create a static instance of the html-to-text compile object so it only compiles once. The fromString() method used in the course is deprecated so this is the new way.
+  static convertHtmlToText = compile({
+    wordwrap: 130,
+    selectors: [
+      { selector: 'a', options: { hideLinkHrefIfSameAsText: true } },
+      { selector: 'img', format: 'skip' }, // Drops raw image tags from plain text fallback
+    ],
+  });
+  //user is self evident but url is for any button links in the emails
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = process.env.MAILTRAP_FROM;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //create sendGrid
+      return;
+    }
+    return nodemailer.createTransport({
+      host: process.env.MAILTRAP_HOST,
+      port: process.env.MAILTRAP_PORT,
+      auth: {
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PW,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    //render pug template, not like in our view controllers with res.render()
+    const html = pug.renderFile(`${__dirname}/../views/emails/${template}`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+    //use our static html-to-text
+    const text = Email.convertHtmlToText(html);
+    //define options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      text,
+      html,
+    };
+    //create transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours gang');
+  }
+}
 
 const sendEmail = async (options) => {
   const transport = nodemailer.createTransport({
